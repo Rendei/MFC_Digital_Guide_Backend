205c8fe9eed27999ad243317e1857548d8460e0f
Arseniy <cenz1217@gmail.com>
2025-03-30 21:05:57 +0500

    added model-metrics get endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
+import time
 from fastapi import FastAPI, Query, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy import func, select
+from app.metrics.get_roadmap_metrics import get_average_metrics_by_model
 from app.schemas import RequestModel
 from app.database import get_db
 from app.roadmap import generate_roadmap_batch, generate_roadmap_livetime
 from app.search import hybrid_search
-from app.models import SearchQuery, Roadmap
+from app.models import SearchQuery, Roadmap, Metrics
 
 
 app = FastAPI()
@@ -28,13 +32,42 @@ async def roadmap_endpoint(data: RequestModel, db: AsyncSession = Depends(get_db
     """
     Generates a roadmap (step-by-step tasks) based on a user request and a specified document.
     """
-    roadmap_text = generate_roadmap_batch(data.document_id, data.user_request)
-    new_roadmap = Roadmap(document_id=data.document_id, user_request=data.user_request, roadmap_text=roadmap_text)
+    # stmt = select(Roadmap).order_by(func.random()).limit(1)
+    # result = await db.execute(stmt)
+    # roadmap_text = result.scalars().first().roadmap_text
+    #model_name = "deepseek-ai/DeepSeek-V3-0324"
+
+    start_time = time.time()
     
-    db.add(new_roadmap)
-    await db.commit()
+    roadmap_text, metrics = generate_roadmap_batch(data.document_id, data.user_request)
     
+    generation_time = time.time() - start_time
+    
+    new_roadmap = Roadmap(document_id=data.document_id, user_request=data.user_request, roadmap_text=roadmap_text, model_name=data.model_name)
+
+    new_metrics = Metrics(
+        bleu_score=metrics["BLEU"],
+        rouge_1_f1=metrics["ROUGE-1 F1"],
+        rouge_2_f1=metrics["ROUGE-2 F1"],
+        rouge_l_f1=metrics["ROUGE-L F1"],
+        bert_score_f1=metrics["BERTScore F1"],
+        generation_time_sec=generation_time
+    )
+
+    async with db.begin():
+        db.add(new_roadmap)
+        await db.flush()
+        new_metrics.roadmap_id = new_roadmap.id
+        db.add(new_metrics)
+
     return {"status": "success", "roadmap": roadmap_text}
+
+
+@app.get("/model-metrics/")
+async def get_model_metrics(db: AsyncSession = Depends(get_db)):
+    metrics = await get_average_metrics_by_model(db)
+    return {"metrics": metrics}
+
 
 if __name__ == "__main__":
     import uvicorn
--- app/schemas.py
+++ app/schemas.py
@@ -4,3 +4,4 @@ from pydantic import BaseModel
 class RequestModel(BaseModel):
     document_id: str
     user_request: str
+    model_name: str = "Meta-Llama-3.3-70B-Instruct-Turbo"
